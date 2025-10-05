36398c47cc7f7130a04651042082bfd388c97276
Mery Minasyan <mery@MacBook-Air-von-Mery.local>
2025-10-05 17:20:37 +0400

    sumexp.js

---
+++ findind.js
@@ -0,0 +1,7 @@
+let numList = [3, 6, 9, 12];
+
+for(let i = 0; i < numList.length; i++){
+   if(numList[i] == 9){
+     console.log(i);
+   }
+}
---
+++ sumexp.js
@@ -0,0 +1,8 @@
+let expenses = [50, 75, 100];
+
+let sum = 0;
+for(let i = 0; i < expenses.length; i++){
+   sum += expenses[i];
+}
+
+console.log("Sum is " + sum);
